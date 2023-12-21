54fb49256410cf411a55a0725941b82d4a4bf1cc
tokkissi <alal4674@gmail.com>
2023-12-22 00:07:31 +0900

    refactor: convert navigation bar from sticky to fixed positioning and add logo image

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function NavigationBar() {
   return (
-    <nav className="flex px-8 text-xl justify-between items-center sticky bg-transparent top-0 mb-4 z-50 h-10 w-full">
-      {/* 좌측 배너 */}
+    <nav className="flex px-8 text-xl justify-between items-center fixed bg-black top-0 z-50 h-12 w-full ">
+      {/* 좌측 로고 */}
       <div>
-        <a href="/">로고 이미지 예정</a>
+        <a href="/">
+          <Image
+            src="/tokkissi-logo.svg"
+            alt="토끼씨 로고"
+            width={35}
+            height={35}
+          />
+        </a>
       </div>
       {/* 우측 네비게이션 */}
       <div className="flex gap-6 font-bold">
