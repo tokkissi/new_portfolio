eaaf0a95106dd84e8d198324909b9a11dd913259
tokkissi <alal4674@gmail.com>
2023-12-25 13:19:29 +0900

    feat: add SkillCardSlider component(#16)
    closes #16

---
+++ src/components/SkillCardSlider.tsx/SkillCardSlider.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import Image from "next/image";
+import { useRef } from "react";
+import { skillDataList } from "../Skills/skillsData";
+
+type SkillCardSliderProps = {
+  reverse?: boolean;
+};
+
+export default function SkillCardSlider({
+  reverse = false,
+}: SkillCardSliderProps) {
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const loadedImageCount = useRef(0);
+
+  const handleMouseEnter = () => {
+    if (carouselRef.current) {
+      carouselRef.current.style.animationPlayState = "paused";
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (carouselRef.current) {
+      carouselRef.current.style.animationPlayState = "running";
+    }
+  };
+
+  // useEffect로 슬라이더를 재생하면 이미지 로드가 모두 되지 않은 상태에서 이미지의 총 가로 너비를 계산하므로 너비 계산에 오차가 생겨 순환이 끊김
+  // 그러므로 이미지가 로드되면 카운트 하고, 총 이미지 수만큼 모두 로드 되었을때 동적으로 총 너비를 계산해야 함
+  const handleImageLoad = () => {
+    loadedImageCount.current += 1;
+
+    if (carouselRef.current) {
+      // Array.from(carouselRef.current.children)는 이 컨테이너 내의 모든 자식 요소들(이미지들)을 배열로 변환함
+      // .reduce((total, child) => total + child.getBoundingClientRect().width, 0)는 이 배열의 각 요소(이미지)에 대해 getBoundingClientRect().width를 호출하여 그 너비를 합함
+
+      if (loadedImageCount.current === skillDataList.length * 2) {
+        const totalWidth = Array.from(carouselRef.current.children).reduce(
+          (total, child) => total + child.getBoundingClientRect().width,
+          0
+        );
+
+        // 자연스럽게 순환하며 무한 재생하기 위해서 이미지 배열을 2배로 한 후 절반에서 시작하도록 했음. 현재 이미지 크기로는 2배로 했을때 순환시키면 화면의 가로 크기가 가득 차기 때문임
+        const translateValue = `-${totalWidth / 2}px`;
+
+        // 컨테이너 너비 계산 후, css 변수로 설정함
+        carouselRef.current.style.setProperty(
+          "--carousel-translate",
+          translateValue
+        );
+      }
+    }
+  };
+
+  return (
+    <div className="py-2">
+      {/* hover시 일시 정지를 위해 skill-cardSlider를 global.css에 정의함 */}
+      <div
+        ref={carouselRef}
+        className={`flex ${
+          reverse ? "animate-reverseSkillCardSlide" : "animate-skillCardSlide"
+        }`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {skillDataList.map((skillData, index) => (
+          <Image
+            key={index}
+            className="rounded-3xl px-2"
+            src={skillData.image}
+            alt={skillData.skillName}
+            width={150}
+            height={150}
+            onLoad={handleImageLoad}
+          />
+        ))}
+
+        {/* 무한 순환을 위한 추가 데이터 */}
+        {skillDataList.map((skillData, index) => (
+          <Image
+            key={index + skillDataList.length}
+            className="rounded-3xl px-2"
+            src={skillData.image}
+            alt={skillData.skillName}
+            width={150}
+            height={150}
+            onLoad={handleImageLoad}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        // translateX(var(--carousel-translate)) 는 SkillCardSlider 컴포넌트의 슬라이더 컨테이너의 가로 길이의 절반 * -1 값임
+
+        // 초기 위치에서 전체 길이의 절반만큼 좌측으로 이동함
+        skillCardSlide: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(var(--carousel-translate))" },
+        },
+        // 절반만큼 좌측으로 이동된 위치에서 우측으로 이동하여 초기 위치로 이동함
+        reverseSkillCardSlide: {
+          "0%": { transform: "translateX(var(--carousel-translate))" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        skillCardSlide: "skillCardSlide 35s linear infinite",
+        reverseSkillCardSlide: "reverseSkillCardSlide 35s linear infinite",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
