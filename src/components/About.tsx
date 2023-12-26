b5faa860f649e0b9de7b238b8c4526d689eab145
tokkissi <alal4674@gmail.com>
2023-12-26 23:04:48 +0900

    style: update HTML structure and CSS for improved About layout (#24)
    close #24

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,32 +1,37 @@
 import Image from "next/image";
 
+const aboutText = `안녕하세요! 항상 긍정적인 마인드를 가지고
+소통하려고 노력하는 프론트엔드 개발자 김제원입니다`;
+
+// 프론트엔드 개발자로서 소통의 중요성을 잘 알고 있습니다.
+//        혼자 벽만 보며 개발하는 개발자가 아닌 팀원과 함께 성장하는
+//         개발자가 되고자 합니다.
+
 export default function About() {
   return (
     <section className="flex items-center h-screen">
-      <div className="w-full flex">
-        <div className="w-3/5">
+      <div className="w-full h-fit flex">
+        <div className="basis-[15%]"></div>
+
+        {/* 좌측 텍스트 영역 */}
+        <div className="basis-[48%] flex flex-col justify-center">
           <h2 className="text-[8rem] mb-8">About</h2>
-          <p className="text-6xl"></p>
-          <p className="text-3xl mb-8">
-            안녕하세요! &nbsp; 항상 <strong>긍정적인 마인드</strong>로{" "}
-            <strong>소통</strong>하기 위해서 노력하는 <strong>김제원</strong>
-            입니다.
-          </p>
-          <p className="text-2xl leading-10">
-            프론트엔드 개발자로서 소통의 중요성을 잘 알고 있습니다.
-            <br /> 혼자 벽만 보며 개발하는 개발자가 아닌 팀원과 함께 성장하는
-            개발자가 되고자 합니다.
-          </p>
+          <div className=" text-2xl mb-8 leading-[3rem] whitespace-pre-wrap">
+            {aboutText}
+          </div>
         </div>
-        <div className="w-2/5 flex justify-center items-center px-8">
+
+        {/* 우측 이메일 전송 폼 영역 */}
+        <div className="basis-[27%] flex justify-start items-center">
           <Image
-            className="border-8 border-white rounded-2xl"
+            className="border-4 border-white rounded-md"
             src="/김제원_프로필_사진.jpg"
             alt="김제원 프로필 사진"
-            width={500}
-            height={500}
+            width={320}
+            height={320}
           />
         </div>
+        <div className="basis-[10%]"></div>
       </div>
     </section>
   );
