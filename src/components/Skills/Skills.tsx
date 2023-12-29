5cd7048156472b03d111f43b69243e14f94d88dc
tokkissi <alal4674@gmail.com>
2023-12-30 05:21:16 +0900

    style: add overflow hidden to skills section

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,7 +3,7 @@ import SkillCardGallery from "../SkillCardGallery";
 
 export default function Skills() {
   return (
-    <section className="">
+    <section className="overflow-hidden">
       <div className="scroll-point h-12 w-full" id="skills"></div>
       <div className="flex items-end mb-16">
         <h2 className="mr-16 leading-none">Skills</h2>
