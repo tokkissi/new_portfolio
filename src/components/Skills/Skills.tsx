f56fabbbc67f8b41e98d35701da29a8627cc86dd
tokkissi <alal4674@gmail.com>
2023-12-30 05:20:38 +0900

    fix: adjust scroll position in skills component

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,7 +3,8 @@ import SkillCardGallery from "../SkillCardGallery";
 
 export default function Skills() {
   return (
-    <section className="scroll-mt-12" id="skills">
+    <section className="">
+      <div className="scroll-point h-12 w-full" id="skills"></div>
       <div className="flex items-end mb-16">
         <h2 className="mr-16 leading-none">Skills</h2>
         <SkillToggleButton />
